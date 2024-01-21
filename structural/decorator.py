7687bbc225d8c4ac3de5d700fe6f135f638cba38
edpyt <edrsa1321s@gmail.com>
2024-01-21 19:02:56 +0300

    Update Decorator Pattern

--- structural/decorator.py
+++ structural/decorator.py
@@ -1,34 +1,128 @@
-"""Подменить реализацию класса"""
+"""
+Декоратор - структурный паттерн проектирования, который позволяет динамически
+добавлять объектам новую функциональность, оборачивая их в полезные «обёртки».
+"""
+from typing import Any, Protocol
 
 
-class TextTag:
-    def __init__(self, text: str) -> None:
-        self.text = text
+class DataSource(Protocol):
+    """Общий интерфейс компонентов"""
 
-    def render(self) -> str:
-        return self.text
+    def write_data(self, data: Any) -> None:
+        ...
 
-
-class BoldWrapper(TextTag):
-    def __init__(self, wrapper: TextTag) -> None:
-        self._wrapper = wrapper
-
-    def render(self) -> str:
-        return f'<b>{self._wrapper.render()}</b>'
+    def read_data(self) -> None:
+        ...
 
 
-class ItalicWrapper(TextTag):
-    def __init__(self, wrapper: TextTag) -> None:
-        self._wrapper = wrapper
+class FileDataSource(DataSource):
+    """Один из конкретных компонентов, реализует базовую функциональность"""
 
-    def render(self) -> str:
-        return f'<i>{self._wrapper.render()}</i>'
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
 
+    def write_data(self, data: Any) -> None:
+        print('Запись данных в файл')
+
+    def read_data(self) -> None:
+        print('Прочитать данные из файла')
+
+
+class DataSourceDecorator(DataSource):
+    """Родитель всех декораторов"""
+
+    _wrapee: DataSource
+
+    def __init__(self, source: DataSource) -> None:
+        self._wrapee = source
+
+    def write_data(self, data: Any) -> None:
+        self._wrapee.write_data(data)
+
+    def read_data(self) -> None:
+        self._wrapee.read_data()
+
+
+class EncryptionDecorator(DataSourceDecorator):
+    """
+    Конкретные декораторы добавляют что-то своё к базовому поведению
+    обёрнутого компонента.
+    """
+
+    _wrapee: DataSource
+
+    def __init__(self, source: DataSource) -> None:
+        self._wrapee = source
+
+    def write_data(self, data: Any) -> None:
+        """
+        1. Зашифровать поданные данные
+        2. Передать зашифрованные данные в метод write_data обёрнутого объекта
+            (_wrapee)
+        """
+        ...
+
+    def read_data(self) -> Any:
+        """
+        1. Получить данные из метода read_data обёрнутого объекта (_wrapee).
+        2. Распаковать их, если они зашифрованны.
+        3. Вернуть результат.
+        """
+        ...
+
+
+class CompressionDecorator(DataSourceDecorator):
+    def write_data(self, data: Any) -> None:
+        """
+        1. Запаковать поданные данные
+        2. Передать запакованные данные в метод write_data обёрнутого объекта
+            (_wrapee)
+        """
+        ...
+
+    def read_data(self) -> None:
+        """
+        1. Получить данные из метода read_data обёрнутого объекта (_wrapee).
+        2. Распаковать их, если они запакованные.
+        3. Вернуть результат.
+        """
+        ...
+
+
+class SalaryManager:
+    source: DataSource
+
+    def __init__(self, source: DataSource) -> None:
+        self.source = source
+
+    def load(self) -> None:
+        return self.source.read_data()
+
+    def save(self) -> None:
+        self.source.write_data(11_000)
+ 
 
 if __name__ == '__main__':
-    text = TextTag('hello!')
-    italic_text = ItalicWrapper(text)
-    bold_italic_text = BoldWrapper(ItalicWrapper(text))
+    source = FileDataSource('somefile.py')
+    source.write_data('print("Hello World!")')
 
-    print(italic_text.render())
-    print(bold_italic_text.render())
+    source = CompressionDecorator(source)
+
+    source = EncryptionDecorator(source)
+
+    source.write_data(11_000)
+
+    # Пример 2 ---------------------------------
+
+    source = FileDataSource('salary.data')
+
+    enabled_encryption = True
+    enabled_compression = True
+
+    if enabled_encryption:
+        source = EncryptionDecorator(source)
+    if enabled_compression:
+        source = CompressionDecorator(source)
+
+    logger = SalaryManager(source)
+    salary = logger.load()
