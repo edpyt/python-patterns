6245badb47b6790dadb426f3e231c571c5c11a7a
edpyt <edrsa1321s@gmail.com>
2024-01-22 20:54:17 +0300

    Update Facade pattern

--- structural/facade.py
+++ structural/facade.py
@@ -1,4 +1,6 @@
-"""Служит для того, чтобы предоставить удобный унифицированный интерфейс
+"""
+Предоставляет простой интерфейс к сложной системе классов, библиотеке или
+фреймворку.
 
 Паттерны Фасад и Адаптер на первый взгляд кажутся похожими. Разница в том,
 что Фасад надстраивает простой интерфейс поверх сложного, а Адаптер
@@ -7,33 +9,66 @@
 """
 
 
-class CPU:
-    def execute(self) -> None:
-        print('Start CPU')
+class File:
+    filename: str
+
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def save(self) -> None:
+        ...
 
 
-class SolidStateDriver:
-    def run(self) -> None:
-        print('Run SSD')
+class VideoFile(File):
+    ...
 
 
-class Memory:
-    def load(self) -> None:
-        print('Load Memory')
+class OggCompressionCode:
+    ...
 
 
-class ComputerFacade:
-    def __init__(self) -> None:
-        self._cpu = CPU()
-        self._ssd = SolidStateDriver()
-        self._memory = Memory()
+class MPEG4CompressionCode:
+    ...
 
-    def start(self) -> None:
-        self._cpu.execute()
-        self._memory.load()
-        self._ssd.run()
+
+class CodecFactory:
+    def extract(self) -> None:
+        ...
+
+
+class BitrateReader:
+    @classmethod
+    def read(cls, filename: str, source_codec) -> None:
+        ...
+
+    @classmethod
+    def convert(cls, buffer: str, destination_codec) -> None:
+        ...
+
+
+class AudioMixer:
+    def fix(self, *args) -> None:
+        ...
+
+
+# Вместо этого мы создаём Фасад - простой интерфейс для работы со сложным
+# фреймворком. Фасад не имеет всей функциональности фреймворка, но зато
+# скрывает его сложность от клиентов.
+class VideoConverter:
+    def convert(self, filename: str, format: str) -> File:
+        file = VideoFile(filename)
+        source_codec = CodecFactory.extract(file)
+        if format == 'mp4':
+            destination_codec = MPEG4CompressionCode()
+        else:
+            destination_codec = OggCompressionCode()
+        buffer = BitrateReader.read(filename, source_codec)
+        result = BitrateReader.convert(buffer, destination_codec)
+        result = AudioMixer().fix(result)
+        return File(result)
 
 
 if __name__ == '__main__':
-    computer = ComputerFacade()
-    computer.start()
+    converter = VideoConverter()
+    mp4 = converter.convert('funny-cats.ogg', 'mp4')
+    mp4.save()
